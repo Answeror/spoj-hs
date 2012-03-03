65d945f623011c2a56efa4472e541753638d17fa
answeror <answeror@gmail.com>
2012-03-03 13:52:28 +0800

    PALIN TLE

--- PALIN.hs
+++ PALIN.hs
@@ -3,38 +3,26 @@
 -- digits.
 
 import Data.Maybe
-import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BS
 
 main = do
     input <- BS.getContents
     mapM_ (BS.putStrLn . solve) (tail $ BS.lines input)
 
 solve :: BS.ByteString -> BS.ByteString
-solve input = let k = fst . fromJust $ BS.readInteger input
-                  n = k+1
-                  in next $ BS.pack $ show n
+solve = next . inc
+
+inc :: BS.ByteString -> BS.ByteString
+inc = BS.pack . show . (1+) . fst . fromJust . BS.readInteger
 
 next :: BS.ByteString -> BS.ByteString
-next n
-    | even $ BS.length n = wheneven
-    | otherwise = whenodd
+next ns = if BS.reverse hs >= ls
+            then make hs
+            else make (inc hs)
     where
-        wheneven :: BS.ByteString
-        wheneven
-            | (BS.reverse highs) >= lows = BS.concat [highs,BS.reverse highs]
-            | BS.length inchighs == BS.length highs = BS.concat [inchighs,BS.reverse inchighs]
-            | otherwise = BS.concat [inchighs,BS.tail (BS.reverse inchighs)]
-        whenodd :: BS.ByteString
-        whenodd
-            | (BS.reverse highs) >= lows = BS.concat [highmids,BS.reverse highs]
-            | BS.length inchighmids == BS.length highmids = BS.concat [inchighmids,BS.tail (BS.reverse inchighmids)]
-            | otherwise = BS.concat [inchighmids,BS.tail (BS.tail (BS.reverse inchighmids))]
-        (highs,mids,lows) = sp n
-        highmids = BS.concat [highs,mids]
-        inchighs = BS.pack $ show ((fst $ fromJust $ BS.readInteger highs)+1)
-        inchighmids = BS.pack $ show ((fst $ fromJust $ BS.readInteger highmids)+1)
-        sp :: BS.ByteString -> (BS.ByteString,BS.ByteString,BS.ByteString)
-        sp n
-            | even $ BS.length n = (BS.take half n,BS.pack [],BS.drop half n)
-            | otherwise = (BS.take half n,BS.take 1 $ BS.drop half n,BS.drop (half+1) n)
-            where half = (BS.length n) `div` 2
+        (hs,ls) = if even (BS.length ns)
+                     then (BS.take half ns, BS.drop half ns)
+                     else (BS.take (half+1) ns, BS.drop half ns)
+        half = (BS.length ns) `div` 2
+        make s = s `BS.append` (BS.drop (BS.length s-half) (BS.reverse s))
+
